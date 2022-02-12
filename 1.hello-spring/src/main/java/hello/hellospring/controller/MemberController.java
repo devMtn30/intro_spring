8206790ec5829d7c85f5cc510d97b602cd9ab3a2
[devmtn30] <[devmtn30@gmail.com]>
2022-02-12 23:57:22 +0900

    회원 관리 예제 - 웹 MVC 개발

--- 1.hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ 1.hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,15 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -14,4 +21,26 @@ public class MemberController {
     }
 
 
+    @GetMapping("members/new")
+    public String CreateForm(){
+        return "/members/creatememberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> memberList = memberService.findMembers();
+        model.addAttribute("members",memberList);//찾은 모델 리스트를 model에 담아서 화면에 넘긴다.
+
+        return "members/memberList";
+    }
 }
